b4138d22c0fa82d85ef08764e1acb896d752cb67
Andy <andy22wu@gmail.com>
2022-03-07 20:29:38 -0600

    Apply master layout page style to owners and vet index pages. Closes #20 and #21.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -14,7 +14,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/","/index", "/index.html"})
+    @RequestMapping({"", "/find", "/","/index", "/index.html"})
     public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
 
