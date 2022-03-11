4435629048d4d766b366b34df28f34b76d620953
Andy <andy22wu@gmail.com>
2022-03-10 21:36:11 -0600

    Convert Vets and Specialty data models to JPA entity. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,6 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
+    @JoinColumn(name = "description")
     private String description;
 
     public String getDescription() {
