24bc61c44463f223182e5551af5d22d1e88d7cd2
Andy <andy22wu@gmail.com>
2022-03-30 15:23:41 -0500

    Add map based visit service. CLoses #43

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> {
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet().getId() == null || visit.getPet() == null || visit.getPet().getOwner() == null) {
+            throw new RuntimeException("Invalid Visit. Needs associated pet and pet owner.");
+        };
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
